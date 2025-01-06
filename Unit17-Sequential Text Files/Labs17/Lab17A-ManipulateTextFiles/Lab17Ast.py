105ba6e7cff34e2f9f17aa4243db792d621f833d
boostmeister12324535664 <150957967+boostmeister12324535664@users.noreply.github.com>
2025-01-06 13:23:34 -0600

    Add files via upload

---
+++ Unit17-Sequential Text Files/Labs17/Lab17A-ManipulateTextFiles/Lab17Ast.py
@@ -0,0 +1,55 @@
+# Lab17Ast.py
+# "Manipulating Text Files"
+# This is the student, starting version of Lab 17A.
+# At minimum, students need to implement the
+# <readData> and <displayData> procedures.
+
+
+def heading():
+   print()
+   print("********************************************")
+   print("Lab 17A, Manipulating Text Files")
+   print("60 Point Version")
+   print("By: JOHN SMITH") # Substitute your own name here.
+   print("********************************************")
+   print("\n")
+
+
+def readData(studentNames,studentGPAs):
+   pass  
+
+
+def displayData(studentNames,studentGPAs): 
+   pass 
+
+
+def computeAndDisplayAverageGPA(studentGPAs):
+   pass
+   
+   
+def capitalizeNames(studentNames):   
+   pass 
+      
+      
+def writeData(studentNames,studentGPAs):  
+   pass         
+
+
+def sortDataByGPA(studentNames,studentGPAs):   
+   pass 
+            
+            
+               
+##########
+#  MAIN  #
+##########
+
+heading() 
+studentNames = []
+studentGPAs = []
+readData(studentNames,studentGPAs)        # Do for  60 point version
+capitalizeNames(studentNames)             # Do for  80 point version
+sortDataByGPA(studentNames,studentGPAs)   # Do for 110 point version
+displayData(studentNames,studentGPAs)     # Do for  60 point version
+computeAndDisplayAverageGPA(studentGPAs)  # Do for  70 point version 
+writeData(studentNames,studentGPAs)       # Do for 90/100 point version
---
+++ Unit17-Sequential Text Files/Labs17/Lab17B-GraphicsBackgroundFromTextFile/Lab17Bst.py
@@ -0,0 +1,144 @@
+# Lab17Bst.py
+# "Creating Graphics Background Text Files"
+# This provided file is identical to TextFiles23.py
+# It will ask the user to enter the name of a text file, 
+# and then use that text file to display a graphics 
+# background. Students are provided with 3 example files:
+# DK1.txt, DK2.txt, and DK3.txt -- which resemble stages 
+# from Nintendo's Donkey Kong arcade game.
+# Students are also given 2 blank files DK4.txt and DK5.txt
+# In these files, they will design their own Dinkey Kong stages.
+
+# NOTE: STUDENTS DO NOT MAKE ANY CHANGES TO THIS FILE
+#       UNLESS YOU ARE DOING THE 110 POINT VERSION!!!
+
+
+from Graphics import *
+import os
+
+
+def convert(q):
+   return q * 20
+
+
+def drawSpace(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("black")
+   fillRect(x,y,20,20)
+
+
+def drawGirder(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("red");
+   fillRect(x,y,20,20)
+   setColor("black")
+   fillCircle(x+10,y+9,6)
+   
+   
+def drawLadder(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("black")
+   fillRect(x,y,20,20)
+   setColor("white")
+   fillRect(x,y,3,20)
+   fillRect(x+16,y,4,20)
+   fillRect(x,y+8,20,4)
+
+
+def drawHammer(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("black")
+   fillRect(x,y,20,20)
+   setColor(150,100,15)
+   fillRect(x,y,20,10)
+   setColor("yellow");
+   fillRect(x+8,y+10,4,10)
+
+
+def drawBarrel(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("black")
+   fillRect(x,y,20,20)
+   setColor(150,100,15)
+   fillRect(x+5,y,10,20)
+   fillArc(x+5,y+10,5,10,180,360)
+   fillArc(x+15,y+10,5,10,0,180)
+   setColor("black")
+   drawLine(x+16,y+9,x+16,y+13)
+   drawLine(x+15,y+6,x+15,y+16)
+   setColor("white")
+   drawLine(x+5,y+4,x+5,y+14)
+   drawLine(x+4,y+7,x+4,y+10)
+   setColor(211,211,211)
+   drawLine(x+5,y,x+15,y)
+
+
+def drawLock(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("cyan")
+   fillRect(x,y,20,5)
+   setColor("yellow")
+   fillRect(x,y+5,20,15)  
+
+
+def drawPole(r,c):
+   x = convert(c)
+   y = convert(r)
+   setColor("black")
+   fillRect(x,y,20,20)
+   setColor("cyan")
+   fillRect(x+7,y,6,20)
+
+
+def drawUnknown(r,c):  
+   x = convert(c)
+   y = convert(r)
+   setColor("pink")
+   fillRect(x,y,20,20)
+   setColor("black")
+   drawString("?",x+3,y+25,"Courier",16,"bold")
+
+
+
+##########
+#  MAIN  #
+##########
+
+numRows = 35
+numCols = 65
+fileName = textinput("Graphics Background","Enter the name of the text file.")
+while not os.path.exists(fileName):
+   fileName = textinput("File does not exist.","Enter the name of the text file.")
+file = open(fileName,"r")
+background = file.readlines()
+file.close()
+
+beginGrfx(1300,700)
+
+for r in range(numRows):
+   for c in range(numCols):
+      if background[r][c] == '.':
+         drawSpace(r,c)
+      elif background[r][c] == '=':
+         drawGirder(r,c)     
+      elif background[r][c] == '#':
+         drawLadder(r,c)     
+      elif background[r][c] == 'H':
+         drawHammer(r,c)     
+      elif background[r][c] == 'B':
+         drawBarrel(r,c) 
+      elif background[r][c] == '*':
+         drawLock(r,c)     
+      elif background[r][c] == '|':
+         drawPole(r,c)    
+      else:
+         drawUnknown(r,c)  
+   update()                                   
+
+endGrfx()
